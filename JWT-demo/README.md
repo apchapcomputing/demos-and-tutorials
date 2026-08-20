d5f7a187ce9736016cefb305ea48eb4f57f2b229
Ashlyn Chapman <apchapcomputing@gmail.com>
2026-08-19 20:48:26 -0400

    preserve history

--- JWT-demo/README.md
+++
@@ -1,4 +0,0 @@
-# JWT-Demo
-
-Based on https://github.com/WebDevSimplified/JWT-Authentication
-This repo is where I work through any changes I will be making to the Nextstrain project.
--- spotify-api-demo/client/src/components/Artwork.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Artwork({ artwork }) {
-    return (
-        <div>
-            <img src={artwork} style={{ height: 250 }} alt="~playlist art~" />
-        </div>
-    )
-}
